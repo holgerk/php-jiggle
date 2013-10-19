db7741c73ec682cae0e3488894cb6e3e653b2df4
Holger Kohnen <h.kohnen@gmail.com>
2013-10-19 14:14:09 +0200

    improve test naming

--- test/JiggleTest.php
+++ test/JiggleTest.php
@@ -2,6 +2,16 @@
 
 require_once __DIR__ . '/../lib/Jiggle.php';
 
+class D3 {
+    public function __construct($d1, $d2) {
+        $this->d1 = $d1;
+        $this->d2 = $d2;
+    }
+    public function sum() {
+        return $this->d1 + $this->d2;
+    }
+}
+
 class JiggleTest extends PHPUnit_Framework_TestCase {
 
     public function testThatSetDepsCouldBeGet() {
@@ -10,21 +20,64 @@ class JiggleTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(42, $jiggle->d1);
     }
 
-    public function testThatDepsCouldBeCreatedViaFactoriesLazily() {
+    public function testThatDepsCouldBeFactoryFunctions() {
         $jiggle = new Jiggle;
         $jiggle->d1 = function () {
-            static $callCount;
-            if (is_null($callCount)) {
-                $callCount = 0;
-            }
+            return 42;
+        };
+        $this->assertEquals(42, $jiggle->d1);
+    }
+
+    public function testThatDepsCouldBeWiredWithoutMagic() {
+        $jiggle = new Jiggle;
+        $jiggle->d1 = 42;
+        $jiggle->d2 = function () use($jiggle) {
+            return $jiggle->d1;
+        };
+        $this->assertEquals(42, $jiggle->d2);
+    }
+
+    public function testThatDepsCouldBeWiredWithMagicFactoryInjection() {
+        $jiggle = new Jiggle;
+        $jiggle->d1 = 42;
+        $jiggle->d2 = function ($d1) {
+            return $d1;
+        };
+        $this->assertEquals(42, $jiggle->d2);
+    }
+
+    public function testClassInstantiationWithoutMagic() {
+        $jiggle = new Jiggle;
+        $jiggle->d1 = 40;
+        $jiggle->d2 = 2;
+        $jiggle->d3 = function () use($jiggle) {
+            return new D3($jiggle->d1, $jiggle->d2);
+        };
+        $this->assertEquals(42, $jiggle->d3->sum());
+    }
+
+    public function testThatFactoriesCalledOnlyOnce() {
+        $jiggle = new Jiggle;
+        $callCount = 0;
+        $jiggle->d1 = function () use(&$callCount) {
             $callCount++;
-            if ($callCount > 1) {
-                throw new Exception("Unexpected second call");
-            }
             return 42;
         };
         $this->assertEquals(42, $jiggle->d1);
         $this->assertEquals(42, $jiggle->d1);
+        $this->assertEquals(1, $callCount);
+    }
+
+    public function testThatFactoriesCalledLayily() {
+        $jiggle = new Jiggle;
+        $called = false;
+        $jiggle->d1 = function () use(&$called) {
+            $called = true;
+            return 42;
+        };
+        $this->assertFalse($called);
+        $this->assertEquals(42, $jiggle->d1);
+        $this->assertTrue($called);
     }
 
     public function testThatReferencesAreReturned() {
@@ -33,15 +86,6 @@ class JiggleTest extends PHPUnit_Framework_TestCase {
         $number = &$jiggle->d1;
         $number += 21;
         $this->assertEquals(42, $jiggle->d1);
-    }
-
-    public function testThatDepsAreInjectedIntoFactoryFunctions() {
-        $jiggle = new Jiggle;
-        $jiggle->d1 = 42;
-        $jiggle->d2 = function ($d1) {
-            return $d1;
-        };
-        $this->assertEquals(42, $jiggle->d2);
     }
 
 
